8f8da91588e5785aab1e2d4aa7ff294d11009c78
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2017-01-11 17:04:46 +0100

    added listview, try to implement camera

--- Memo/app/src/main/java/io/nicco/memo/memo/frag_list.java
+++ Memo/app/src/main/java/io/nicco/memo/memo/frag_list.java
@@ -3,23 +3,24 @@ package io.nicco.memo.memo;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class frag_list extends Fragment {
 
-    private TextView tvOut;
-    private EditText etIn;
-    private EditText etFile;
-    private Button btn_w;
-    private Button btn_r;
-    private Button btn_n;
-    private Button btn_d;
+    private ListView lv;
     private NoteText nt;
+    SimpleCursorAdapter cursorAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,50 +29,21 @@ public class frag_list extends Fragment {
         final Utils u = new Utils();
         final String id = u.genRandId();
         nt = new NoteText(getContext());
+        lv = (ListView) v.findViewById(R.id.frag_list_list);
 
-        tvOut = (TextView) v.findViewById(R.id.tvOut);
-        etIn = (EditText) v.findViewById(R.id.etIn);
-        etFile = (EditText) v.findViewById(R.id.etFile);
-        btn_w = (Button) v.findViewById(R.id.frag_list_write);
-        btn_r = (Button) v.findViewById(R.id.frag_list_read);
-        btn_n = (Button) v.findViewById(R.id.frag_list_new);
-        btn_d = (Button) v.findViewById(R.id.frag_list_delete);
-
-        btn_r.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tvOut.setText(u.read(c, nt.id, etFile.getText().toString()));
+        ArrayList<String> folders = new ArrayList<>();
+        File f = new File(String.valueOf(getContext().getFilesDir()));
+        File[] files = f.listFiles();
+        for (File inFile : files) {
+            if (inFile.isDirectory() && inFile.length()==32) {
+                folders.add(inFile.getName());
             }
-        });
+        }
 
-        btn_w.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                u.save(c, nt.id, etFile.getText().toString(), etIn.getText().toString());
-            }
-        });
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (getContext(),android.R.layout.simple_list_item_1,folders);
 
-        btn_n.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                nt = new NoteText(getContext());
-            }
-        });
-
-        btn_d.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                nt.delete();
-            }
-        });
+        lv.setAdapter(arrayAdapter);
 
         return v;
-    }
-
-    @Override
-    public void onResume() {
-        tvOut.setText(nt.id);
-
-        super.onResume();
     }
 }
